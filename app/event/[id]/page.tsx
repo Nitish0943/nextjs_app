b180afb90045d6b60be8325e409e57d52e7cf739
Nitish0943 <ntshpthk09@gmail.com>
2025-02-15 22:33:55 +0530

    fixed

--- app/event/[id]/page.tsx
+++ app/event/[id]/page.tsx
@@ -1,7 +1,7 @@
-import EventDetails from "@/app/components/EventDetails"
-
-export default function EventPage({ params }: { params: { id: string } }) {
-  // In a real application, you would fetch the event details using the id
-  return <EventDetails eventId={params.id} />
+interface EventDetailsProps {
+  eventId: string;
 }
 
+export default function EventDetails({ eventId }: EventDetailsProps) {
+  return <div>Event ID: {eventId}</div>;
+}
